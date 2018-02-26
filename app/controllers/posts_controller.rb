0b8506b9a4f0c009b4e052637269063971be850f
github username <github email address>
2018-02-26 09:06:29 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
+  private
+  def find_post
+    @post = Post.find(params[:post])
+  end
 end
